790f946b59731c556ff0e64483eb44a0b47d4891
gnopai <vghype1@gmail.com>
2025-11-28 12:51:30 -0800

    Setup backend to describe available kernels and use a specific one on request, with params.

--- backend/api/kernels.py
+++ backend/api/kernels.py
@@ -1,5 +1,12 @@
 import numpy as np
+import sys
+import inspect
 
+this_module = sys.modules[__name__]
+
+# There's some hacky reflection set up to register kernels automatically, so make sure that:
+# 1) all kernel names end in "Kernel"
+# 2) only use a single (optional) constructor param with a default value set
 
 class GaussianKernel:
     def __init__(self, variance = 1.0):
@@ -20,3 +27,35 @@ class PowerNKernel:
 
     def compute(self, x, y):
         return (np.dot(x, y.T) + 1) ** power
+
+
+def createKernel(name, **kwargs):
+    cls = globals().get(name)
+    if cls is None:
+        raise ValueError('Unknown kernel requested!')
+
+    return cls(**kwargs)
+
+
+def _extractConfigForKernelClass(obj):
+    className, cls = obj
+    if not className.endswith('Kernel'):
+        return None
+
+    paramName = None
+    paramDefault = None
+
+    # Assume single parameters for now with defaults set
+    for name, param, in inspect.signature(cls.__init__).parameters.items():
+        if name != 'self' and param.default is not inspect.Parameter.empty:
+            paramName = name
+            paramDefault = param.default
+
+    return { 'name': className, 'param': paramName, 'paramDefault': paramDefault }
+
+
+def getKernelConfigs():
+    kernelClasses = inspect.getmembers(this_module, inspect.isclass)
+    configs = [_extractConfigForKernelClass(cls) for cls in kernelClasses]
+    return [config for config in configs if config is not None]
+
--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, abort, make_response
 import numpy as np
-from api.kernels import GaussianKernel
+from api.kernels import createKernel, getKernelConfigs
 from api.data import make_fake_data
 from api.util import build_img_src_from_plot
 import api.ridge as ridge
@@ -13,14 +13,41 @@ def health():
     return jsonify({'message': 'OK'})
 
 
-@api_blueprint.route('/ridge')
+@api_blueprint.route('/kernels', methods=['GET'])
+def kernelConfigs():
+    kernelConfigs = getKernelConfigs()
+    return jsonify({'kernelConfigs': kernelConfigs})
+
+
+@api_blueprint.route('/ridge', methods=['POST'])
 def ridge_regression():
     dataset = make_fake_data()
     n = len(dataset[0])
-    kernel = GaussianKernel(variance = 1.0)
+
+    body = request.get_json()
+    kernel = _getKernelForRequest(body)
     lamb = 0.1
+
     ridge_x = np.linspace(0, n, 1000)
     ridge_y = ridge.ridge_regression(kernel, lamb, dataset, ridge_x)
     plot_ridge = lambda plt: plt.plot(dataset[0], dataset[1], 'b.', ridge_x, ridge_y, '-r')
     image_src = build_img_src_from_plot(plot_ridge)
     return jsonify({'message': 'OK', 'img_src': image_src})
+
+
+def _getKernelForRequest(body):
+    kernelParam = body.get('kernelParamName')
+    kernelParamValue = body.get('kernelParamValue')
+    kernelParams = {}
+    if kernelParam and kernelParamValue is not None:
+        kernelParams = { kernelParam: kernelParamValue }
+
+    try:
+        return createKernel(body.get('kernel'), **kernelParams)
+    except:
+        abort(error_response(400, 'Invalid kernel and/or parameters specified'))
+
+
+def error_response(status_code, message):
+    return make_response(jsonify({'message': message}), status_code)
+
